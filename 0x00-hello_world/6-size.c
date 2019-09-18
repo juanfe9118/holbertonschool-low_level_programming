06dc7b12c65fc2042443364feabec1afc9e2fd23
juanfe9118 <1168@holbertonschool.com>
2019-09-18 20:23:20 +0000

    Add 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+/**
+ *main - beginning of program
+ *
+ *Description: prints a string
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
