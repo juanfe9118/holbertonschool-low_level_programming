b3b73afa67467ae8b0eac466ece22ea6231c1ac7
juanfe9118 <1168@holbertonschool.com>
2019-09-18 20:03:20 +0000

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - beginning of program
+ *
+ *Description: prints a string
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
