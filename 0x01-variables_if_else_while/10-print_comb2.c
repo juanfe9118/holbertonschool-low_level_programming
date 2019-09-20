402ac7b3596264cb0fc7fc4c95a473998d0f378a
juanfe9118 <1168@holbertonschool.com>
2019-09-20 04:31:36 +0000

    Add 10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+# include <stdio.h>
+
+/**
+ *main - beginning of program
+ *
+ *Description: prints the digits from 00 to 99 separated by commas
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int n1;
+	int n2;
+
+	for (n1 = 0; n1 < 10; n1++)
+	{
+		for (n2 = 0; n2 < 10; n2++)
+		{
+			putchar((n1 % 10) + '0');
+			putchar((n2 % 10) + '0');
+			if (n1 != 9 || n2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
