1cfd4a46e4e6e836cfab6486244e66580452d4db
juanfe9118 <1168@holbertonschool.com>
2019-09-25 03:40:55 +0000

    Add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *add - beginning of program
+ *
+ *Description: prints the sum of two numbers
+ *
+ *@n:first number of the sum
+ *@o:second number of the sum
+ *
+ *Return: the sum of the numbers
+ */
+int add(int n, int o)
+{
+	int sum = n + o;
+
+	return (sum);
+}
