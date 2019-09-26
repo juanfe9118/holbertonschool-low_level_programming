e972d829b643fb5781b5ce28dc93a6ee7f62a252
juanfe9118 <1168@holbertonschool.com>
2019-09-26 18:42:52 +0000

    Add 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ *main - Beginning of program
+ *
+ *Description: prints the numbers from 1 to 100 but the multiples of 3 will
+ *print "Fizz" instead. Likewise the multiples of 5 will print "Buzz" instead,
+ *the multiples of both 3 and 5 will print "FizzBuzz"
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+		printf(" ");
+	}
+	return (0);
+}
