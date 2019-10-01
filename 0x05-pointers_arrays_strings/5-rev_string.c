bb413dbe58d0bfb317bc930e39f0b9071f0c644a
juanfe9118 <1168@holbertonschool.com>
2019-10-01 19:47:08 +0000

    Add 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *rev_string - reverses a string
+ *
+ *@s: pointer that will store the string's location
+ *
+ *Return: nothing
+ */
+void rev_string(char *s)
+{
+	int begin, pos, tmp;
+
+	pos = 0;
+	while (s[pos] != 0)
+	{
+		pos++;
+	}
+	pos--;
+	for (begin = 0; begin < pos / 2; begin++)
+	{
+		tmp = s[begin];
+		s[begin] = s[pos - begin];
+		s[pos - begin] = tmp;
+	}
+}
