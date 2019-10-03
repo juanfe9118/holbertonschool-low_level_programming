d521fe03d083eb5a39cf821aed08bfc3035efe8f
juanfe9118 <1168@holbertonschool.com>
2019-10-03 15:53:58 +0000

    Add 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ **_strcat - concatenates the string from a pointer to another
+ *
+ *@src: pointer that has the string to be concatenated
+ *@dest: pointer that will have its string concatenated with the other
+ *
+ *Return: strings concatenated to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != 0; i++)
+	{
+	}
+	for (j = 0; src[j] != 0; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	return (dest);
+}
