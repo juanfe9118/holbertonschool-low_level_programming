9e5db0314ad85a941cadaa645f3124c54105ceca
juanfe9118 <1168@holbertonschool.com>
2019-10-21 20:19:35 +0000

    Fix first task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
-#findef DOG_H
+#ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - creates a struct for dogs' information
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
