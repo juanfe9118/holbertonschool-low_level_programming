559516de1322aa371643d21691f8c067bd1d0862
juanfe9118 <1168@holbertonschool.com>
2019-10-21 20:13:50 +0000

    Add directory and first task

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+# This directory is for learning to use _structures and typedef_ in C
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+#findef DOG_H
+#define DOG_H
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+int _putchar(char c);
+
+#endif
