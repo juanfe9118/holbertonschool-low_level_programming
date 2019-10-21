9c4acd94c9cc9fff027e53b11bb9609b62e11928
juanfe9118 <1168@holbertonschool.com>
2019-10-21 21:54:31 +0000

    Modify dog.h for fourth task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,11 @@ struct dog
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+} dog_t;
 
 #endif
