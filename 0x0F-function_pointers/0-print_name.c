9eda33e7cb0e704335ddf8c61bbc4db3a35074ca
juanfe9118 <1168@holbertonschool.com>
2019-10-23 15:09:04 +0000

    Add directory and first task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - prints a name after is has been passed through another function
+ *
+ *@name: name to be prited
+ *@f: pointer to the function that will be used with name
+ *
+ *Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+# This directory is for learning how to use _funtion pointers_ in C
