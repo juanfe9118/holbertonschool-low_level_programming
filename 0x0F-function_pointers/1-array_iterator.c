cafefe462b8303ce15fb4784c0e763349999aa4d
juanfe9118 <1168@holbertonschool.com>
2019-10-23 19:08:09 +0000

    Fix second task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,12 +13,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (action != NULL)
-	{
-		for (i = 0; i < size; i++)
-		{
-			if (array[i] != NULL)
-				action(array[i]);
-		}
-	}
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
