9b410482423d44bf7da71c70609b7ffa0036e029
juanfe9118 <1168@holbertonschool.com>
2019-10-23 18:55:51 +0000

    Fix first task

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,7 @@
-#include <stdlib.h>
-
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
