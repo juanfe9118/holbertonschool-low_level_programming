a4e49fdf827b8779055f4b0d27c59852ba8031a0
juanfe9118 <1168@holbertonschool.com>
2019-10-26 23:42:00 +0000

    Fix second task

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
@@ -19,9 +20,8 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_c(va_list x);
-void print_i(va_list x);
-void print_f(va_list x);
-void print_s(va_list x);
-
+void print_c(va_list);
+void print_i(va_list);
+void print_f(va_list);
+void print_s(va_list);
 #endif
