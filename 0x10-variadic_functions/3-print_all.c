9787ec7a9e83ae21deea0fdb277ffb5a170e2c8d
juanfe9118 <1168@holbertonschool.com>
2019-10-26 17:15:52 +0000

    Fix fourth task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,46 +3,6 @@
 #include <stdio.h>
 
 /**
- * print_all - printing all
- *
- * @format: is a list of types of arguments
- * We can not change the value or pointer
- * Return: Nothing, is void
- */
-void print_all(const char * const format, ...)
-{
-	va_list ar;
-	unsigned int i, j;
-	type func[] = {
-		{'c', print_c},
-		{'i', print_i},
-		{'f', print_f},
-		{'s', print_s},
-	};
-
-	i = 0;
-	va_start(ar, format);
-
-	while (format[i] != '\0')
-	{
-		j = 0;
-		while (j < 4)
-		{
-			if (func[j].func == format[i])
-			{
-				func[j].p(ar);
-				if (format[i + 1] != '\0')
-					printf(", ");
-				break;
-			}
-			j++;
-		}
-		i++;
-	}
-	printf("\n");
-	va_end(ar);
-}
-/**
  *print_c - prints a char
  *@x: va_list passed to the function
  *Return: nothing
@@ -81,4 +41,44 @@ void print_s(va_list x)
 	if (*str == '\0')
 		str = "(nil)";
 	printf("%s", str);
+}
+/**
+ * print_all - printing all
+ *
+ * @format: is a list of types of arguments
+ * We can not change the value or pointer
+ * Return: Nothing, is void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ar;
+	unsigned int i, j;
+	type func[] = {
+		{'c', print_c},
+		{'i', print_i},
+		{'f', print_f},
+		{'s', print_s},
+	};
+
+	i = 0;
+	va_start(ar, format);
+
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (func[j].func == format[i])
+			{
+				func[j].p(ar);
+				if (format[i + 1] != '\0')
+					printf(", ");
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ar);
 }
