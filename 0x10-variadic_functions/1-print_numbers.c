f5b519fda22a024b04d8c47995096f2c5c1673b2
juanfe9118 <1168@holbertonschool.com>
2019-10-26 23:52:55 +0000

    Fix second task

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,16 +11,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list numbers;
 	unsigned int i;
 
-	if (n > 0)
+	if (n == 0)
+		printf("Error\n");
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(numbers, n);
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(numbers, int));
-			if (i < (n - 1))
-				printf("%s", separator);
-		}
-		printf("\n");
-		va_end(numbers);
+		printf("%d", va_arg(numbers, int));
+		if (i < (n - 1))
+			printf("%s", separator);
 	}
+	printf("\n");
+	va_end(numbers);
 }
