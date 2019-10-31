2622b5cb50187103e64cede020957e20a1e36865
juanfe9118 <1168@holbertonschool.com>
2019-10-31 17:14:18 +0000

    Add directory, header and first function

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+# include "lists.h"
+
+/**
+ *print_list - prints all the elements of a list_t list
+ *@h: the list_t list
+ *Return: the amount of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *ptr;
+	size_t i = 0;
+
+	ptr = h;
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+			printf("[0] (nil)");
+		printf("[%d] %s\n", ptr->len, ptr->str);
+		i++;
+		ptr = ptr->next;
+	}
+
+	return (i);
+}
