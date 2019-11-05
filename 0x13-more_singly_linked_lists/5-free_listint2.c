3f697d8c1ed5cf49179158a770d1984d4f27679b
juanfe9118 <1168@holbertonschool.com>
2019-11-05 21:24:52 +0000

    Fix sixth function

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,15 +10,15 @@ void free_listint2(listint_t **head)
 	listint_t *dump, *tmp;
 
 	dump = *head;
-	if (*head != NULL || head != NULL)
+	if (head != NULL)
 	{
-		while (dump != NULL)
+		while (dump->next != NULL)
 		{
-			tmp = dump;
-			dump = dump->next;
-			free(tmp);
+			tmp = dump->next;
+			free(dump);
+			dump = tmp;
 		}
 		free(dump);
+		*head = NULL;
 	}
-	*head = NULL;
 }
