608f2659a855591b373569902ac7e3f6767a3cc4
juanfe9118 <1168@holbertonschool.com>
2019-11-05 21:10:25 +0000

    Add sixth function

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - frees a listint_t list and sets head to NULL
+ *@head: pointer to the listint_t list
+ *Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *dump, *tmp;
+
+	dump = *head;
+	if (head != NULL)
+	{
+		while (dump != NULL)
+		{
+			tmp = dump;
+			dump = dump->next;
+			free(tmp);
+		}
+		free(dump);
+	}
+	*head = NULL;
+}
