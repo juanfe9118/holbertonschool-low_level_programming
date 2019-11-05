5f8bca1df7db3f50414d4406c6686e50cc715a2b
juanfe9118 <1168@holbertonschool.com>
2019-11-05 23:30:42 +0000

    Fix betty in eighth function

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- *get_node_at_index - returns the nth node of a listint_t linked list
+ *get_nodeint_at_index - returns the nth node of a listint_t linked list
  *@head: pointer to the listint_t list
  *@index: index of the node
  *Return: NULL is the node at index doesn't exists, the node at index otherwise
