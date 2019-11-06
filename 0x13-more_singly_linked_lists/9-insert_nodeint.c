561b0dc4dcaac9948ed5121ab0009b912ff59761
juanfe9118 <1168@holbertonschool.com>
2019-11-06 01:17:38 +0000

    Add tenth function

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ *insert_nodeint_at_index - inserts a new node at a given position
+ *@head: pointer to the first node of the listint_t list
+ *@idx: index position of the node to be inserted
+ *@n: value of the new node
+ *Return: the listint_t list after the new node has been inserted
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node, *node_to_insert_after;
+	unsigned int i = 0;
+
+	node_to_insert_after = *head;
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (!(*head))
+		return (NULL);
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
+		while (i < idx-1 && node_to_insert_after)
+		{
+			node_to_insert_after = node_to_insert_after->next;
+			i++;
+		}
+		if (!node_to_insert_after)
+			return (NULL);
+		new_node->next = node_to_insert_after->next;
+		node_to_insert_after->next = new_node;
+	}
+	return (*head);
+}
