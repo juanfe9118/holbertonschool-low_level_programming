4af101ae42010025dcf8be7269c5611972548080
juanfe9118 <1168@holbertonschool.com>
2019-11-12 17:52:44 +0000

    Fix file not being closed at returns

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,13 +16,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		close(fd);
 		return (0);
+	}
 	rd = read(fd, buf, letters);
 	if (rd == -1)
+	{
+		close(fd);
 		return (0);
+	}
 	print = write(STDOUT_FILENO, buf, rd);
 	if (print == -1)
+	{
+		close(fd);
 		return (0);
+	}
 	return (print);
 	free(buf);
 	close(fd);
