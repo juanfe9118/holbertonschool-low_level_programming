a9152c7625fb614efe8fc0e486bf0a04a82fd484
juanfe9118 <1168@holbertonschool.com>
2019-12-14 21:51:19 +0000

    Fix betty and list NULL && idx 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
 /**
+  * new_malloc - allocates memory for a new node and assigns the value
+  * @new: dlistint_t node to be allocated
+  * @n: value to assign to new->n
+  * Return: the allocated node or NULL if failed
+  */
+dlistint_t *new_malloc(dlistint_t **new, int n)
+{
+	*new = malloc(sizeof(dlistint_t));
+	if (*new == NULL)
+		return (NULL);
+	(*new)->n = n;
+	return (*new);
+}
+
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: dlistint_t list to insert node into
  * @idx: index to insert node at
@@ -12,10 +27,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new = NULL, *node_b4_ins = *h;
 	unsigned int i = 1;
 
-	new = malloc(sizeof(dlistint_t));
+	new_malloc(&new, n);
 	if (new == NULL)
 		return (NULL);
-	new->n = n;
+	if (idx == 0 && *h == NULL)
+	{
+		new->next = *h;
+		new->prev = NULL;
+		*h = new;
+		return (new);
+	}
 	if (idx == 0 && *h != NULL)
 	{
 		new->next = *h;
