d8a3e53929c11468fed289d8783e30afe4a76490
juanfe9118 <1168@holbertonschool.com>
2019-12-15 04:26:15 +0000

    Fix conditions in 8th task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,34 +30,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new = NULL, *node_b4_ins = *h;
 	unsigned int i = 1, len = dlistint_len(*h);
 
+	if (idx == 0)
+	{
+		new = add_dnodeint(h, n);
+		return (new);
+	}
+
+	if (*h == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	if (idx == 0 && *h == NULL)
-	{
-		add_dnodeint(h, n);
-		return ((*h));
-	}
-	if (idx == 0 && *h != NULL)
-	{
-		new->next = *h;
-		new->prev = NULL;
-		(*h)->prev = new;
-		*h = new;
-		return (new);
-	}
+
 	if (idx == len)
 	{
 		free(new);
 		new = add_dnodeint_end(h, n);
 		return (new);
 	}
+
 	if (idx > len)
 	{
 		free(new);
 		return (NULL);
 	}
+
 	while (i < idx)
 	{
 		node_b4_ins = node_b4_ins->next;
