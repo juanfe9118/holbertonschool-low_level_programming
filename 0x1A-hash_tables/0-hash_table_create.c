78eafae57a77fd7aa246fbc5ffc80eb3bff84295
juanfe9118 <1168@holbertonschool.com>
2020-01-18 16:00:05 -0500

    Modify first malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = NULL;
 
-	new_table = malloc(sizeof(hash_table_t));
+	new_table = malloc(sizeof(hash_table_t) * 1);
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
