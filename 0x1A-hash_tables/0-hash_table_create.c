7e7684ab5777cc0f22649f3241bdae469869c968
juanfe9118 <1168@holbertonschool.com>
2020-01-18 15:57:41 -0500

    Return changes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,6 +15,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t) * size);
 	if (new_table->array == NULL)
+	{
+		free(new_table);
 		return (NULL);
+	}
 	return (new_table);
 }
