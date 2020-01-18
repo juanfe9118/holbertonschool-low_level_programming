a173608721220386023e5321aa54fa9ffa5d84d4
juanfe9118 <1168@holbertonschool.com>
2020-01-18 16:13:06 -0500

    Add array initialization

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,18 +8,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = NULL;
+	unsigned int i;
 
 	new_table = malloc(sizeof(hash_table_t) * 1);
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
-	if (size == 0)
-		return (new_table);
 	new_table->array = malloc(sizeof(hash_node_t) * size);
 	if (new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 	return (new_table);
 }
