4819b3d99bfef61b2ce7ffc3b862ebb76459fd5d
juanfe9118 <1168@holbertonschool.com>
2020-01-18 16:18:34 -0500

    Fix second malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,19 +8,16 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = NULL;
-	unsigned int i;
 
 	new_table = malloc(sizeof(hash_table_t) * 1);
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t) * size);
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
-		new_table->array[i] = NULL;
 	return (new_table);
 }
