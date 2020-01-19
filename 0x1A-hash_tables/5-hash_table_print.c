2ca4a8aadd695f2db1ec32d120645edc07cdd796
juanfe9118 <1168@holbertonschool.com>
2020-01-19 18:24:42 -0500

    Fix null condition

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,7 +8,7 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned int i;
-	hash_node_t **array = ht->array, *node;
+	hash_node_t *node = NULL;
 	char *sep = "";
 
 	if (ht != NULL)
@@ -16,12 +16,12 @@ void hash_table_print(const hash_table_t *ht)
 		printf("{");
 		for (i = 0; i < ht->size; i++)
 		{
-			node = array[i];
+			node = ht->array[i];
 			while (node != NULL)
 			{
 				printf("%s'%s': '%s'", sep, node->key, node->value);
-				sep = ", ";
 				node = node->next;
+				sep = ", ";
 			}
 		}
 		printf("}\n");
