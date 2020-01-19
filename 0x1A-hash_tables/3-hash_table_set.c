d88f7a2298af07b35fa89f7b2a0869448fcf0ba2
juanfe9118 <1168@holbertonschool.com>
2020-01-19 17:14:10 -0500

    Fix node insertion

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
-	ht->array[index] = new_node->next;
+	ht->array[index] = new_node;
 	return (1);
 
 return (1);
