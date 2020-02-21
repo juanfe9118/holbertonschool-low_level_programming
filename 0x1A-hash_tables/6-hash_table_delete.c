c03e13c639f61f31a83423fe5b1915a9f6dad02d
juanfe9118 <1168@holbertonschool.com>
2020-02-21 16:35:08 -0500

    Modify spacing in delete function

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,24 +7,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *ntd, *nad; /*ntd=node to delete, nad=node after delete*/
-	unsigned int i;
+    hash_node_t *ntd, *nad; /*ntd=node to delete, nad=node after delete*/
+    unsigned int i;
 
-	if (ht != NULL)
-	{
-		for (i = 0; i < ht->size; i++)
-		{
-			ntd = ht->array[i];
-			while (ntd != NULL)
-			{
-				nad = ntd->next;
-				free(ntd->key);
-				free(ntd->value);
-				free(ntd);
-				ntd = nad;
-			}
-		}
-		free(ht->array);
-		free(ht);
-	}
+    if (ht != NULL)
+    {
+        for (i = 0; i < ht->size; i++)
+        {
+            ntd = ht->array[i];
+            while (ntd != NULL)
+            {
+                nad = ntd->next;
+                free(ntd->key);
+                free(ntd->value);
+                free(ntd);
+                ntd = nad;
+            }
+        }
+        free(ht->array);
+        free(ht);
+    }
 }
