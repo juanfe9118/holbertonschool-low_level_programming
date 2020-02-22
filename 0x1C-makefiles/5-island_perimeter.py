7b28c09a88345b0d912d3338b804617560779cf4
juanfe9118 <1168@holbertonschool.com>
2020-02-21 22:28:52 -0500

    Modified the conditions to find edges as well

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,27 +8,12 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                try:
-                    if grid[i][j + 1] == 0:
-                        n += 1
-                except IndexError:
+                if grid[i][j + 1] == 0 or j = (len(grid[0]) - 1):
                     n += 1
-
-                try:
-                    if grid[i][j - 1] == 0:
-                        n += 1
-                except IndexError:
+                if grid[i][j - 1] == 0 or j = 0:
                     n += 1
-
-                try:
-                    if grid[i + 1][j] == 0:
-                        n += 1
-                except IndexError:
+                if grid[i + 1][j] == 0 or i = (len(grid) - 1):
                     n += 1
-
-                try:
-                    if grid[i + 1][j] == 0:
-                        n += 1
-                except IndexError:
+                if grid[i + 1][j] == 0 or i = 0:
                     n += 1
     return n
